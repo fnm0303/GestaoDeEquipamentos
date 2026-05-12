08115b17c4c4a3aeb18c7316849cb793aac8f342
Felipe Neto Mello <fnm03br@gmail.com>
2026-05-12 19:45:34 -0300

    Adicionada classe Equipamento

--- GestaoDeEquipamentos.ConsoleApp/Dominio/Equipamento.cs
+++ GestaoDeEquipamentos.ConsoleApp/Dominio/Equipamento.cs
@@ -2,6 +2,13 @@ using System;
 
 namespace GestaoDeEquipamentos.ConsoleApp.Dominio;
 
+/*
+• Deve ter identificador único (id)
+• Deve ter um nome com no mínimo 6 caracteres;
+• Deve ter um preço de aquisição;
+• Deve ter uma fabricante;
+• Deve ter uma data de fabricação;
+*/
 public class Equipamento
 {
 
