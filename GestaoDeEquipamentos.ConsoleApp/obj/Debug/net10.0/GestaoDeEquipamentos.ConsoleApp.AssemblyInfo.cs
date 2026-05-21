1c2ef272473e93c30f740fd301c1d870e032de34
Felipe Neto Mello <fnm03br@gmail.com>
2026-05-21 19:11:20 -0300

    Encerrada v3

--- GestaoDeEquipamentos.ConsoleApp/obj/Debug/net10.0/GestaoDeEquipamentos.ConsoleApp.AssemblyInfo.cs
+++ GestaoDeEquipamentos.ConsoleApp/obj/Debug/net10.0/GestaoDeEquipamentos.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4444d000b5b37f119d6d1f1d70b445a7aa20ed9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25537f2fef6131c9494a15159baf984e7decf502")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
